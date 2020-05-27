5b8e5a424034cead3ea55c9f6776eaecb71155e5
Ray Villaraza <ray.villaraza@strongmind.com>
2020-05-26 22:56:35 -0700

    Updates

---
+++ precache-manifest.4b8ed3271b69ea85fb021aad5fa427e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0a19ba6b841826f7e6deb8abdb0290b",
+    "url": "/common-react-springs/index.html"
+  },
+  {
+    "revision": "f90ab7e86b725290feea",
+    "url": "/common-react-springs/static/css/main.4d769747.chunk.css"
+  },
+  {
+    "revision": "2d65714e47ee9eceac65",
+    "url": "/common-react-springs/static/js/2.0dae49aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/common-react-springs/static/js/2.0dae49aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f90ab7e86b725290feea",
+    "url": "/common-react-springs/static/js/main.60f6053e.chunk.js"
+  },
+  {
+    "revision": "d43f91a5e9fa5b3af482",
+    "url": "/common-react-springs/static/js/runtime-main.20596e97.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/common-react-springs/precache-manifest.8c05d37faf7ba2ee0f9562ee3ecdc504.js"
+  "/common-react-springs/precache-manifest.4b8ed3271b69ea85fb021aad5fa427e8.js"
 );
 
 self.addEventListener('message', (event) => {
